f065df38717ab6a7553fbf757d0c4efc7cde83bd
alessia vinnik <alessia.vinnik@student.howest.be>
2023-01-25 14:44:50 +0100

    time track tot 24 januari aangepast

--- time-tracking/time.py
+++ time-tracking/time.py
@@ -12,14 +12,24 @@ plt.rcParams["figure.autolayout"] = True
 # 23, 24, 25, 26, 27, 28, 29,
 # 30, 31
 x = [
-    date2num(dt.datetime(2022, 11, 9)),
-    date2num(dt.datetime(2022, 11, 10)),
-    date2num(dt.datetime(2022, 11, 11)),
-    date2num(dt.datetime(2022, 11, 12)),
-    date2num(dt.datetime(2022, 11, 13)),
-    date2num(dt.datetime(2022, 11, 14)),
-    date2num(dt.datetime(2022, 11, 15)),
-    date2num(dt.datetime(2022, 11, 16))
+    date2num(dt.datetime(2023, 1, 9)),
+    date2num(dt.datetime(2023, 1, 10)),
+    date2num(dt.datetime(2023, 1, 11)),
+    date2num(dt.datetime(2023, 1, 12)),
+    date2num(dt.datetime(2023, 1, 13)),
+    date2num(dt.datetime(2023, 1, 14)),
+    date2num(dt.datetime(2023, 1, 15)),
+
+    date2num(dt.datetime(2023, 1, 16)),
+    date2num(dt.datetime(2023, 1, 17)),
+    date2num(dt.datetime(2023, 1, 18)),
+    date2num(dt.datetime(2023, 1, 19)),
+    date2num(dt.datetime(2023, 1, 20)),
+    date2num(dt.datetime(2023, 1, 21)),
+    date2num(dt.datetime(2023, 1, 22)),
+
+    date2num(dt.datetime(2023, 1, 23)),
+    date2num(dt.datetime(2023, 1, 24)),
 ]
 
 # Senne
@@ -34,7 +44,17 @@ uren_S = [
     (5, 7),
     (0, 0),
     (1, 1),
-    (6, 51)
+
+    (6, 51),
+    (6, 33),
+    (6, 40),
+    (6, 8),
+    (3, 19),
+    (0, 0),
+    (0, 0),
+
+    (7, 17),
+    (6, 5)
 ]
 S = [x[0] + round(x[1] / 60, 3) for x in uren_S]
 
@@ -50,7 +70,17 @@ uren_M = [
     (3, 50),
     (0, 20),
     (0, 0),
-    (6, 38)
+
+    (6, 38),
+    (6, 30),
+    (10, 29),
+    (7, 23),
+    (3, 26),
+    (4, 2),
+    (3, 2),
+
+    (8, 53),
+    (7, 55)
 ]
 M = [x[0] + round(x[1] / 60, 3) for x in uren_M]
 
@@ -66,7 +96,17 @@ uren_J = [
     (3, 9),
     (0, 0),
     (0, 0),
-    (7, 38)
+
+    (7, 38),
+    (6, 3),
+    (6, 13),
+    (3, 40),
+    (5, 22),
+    (0, 44),
+    (1, 15),
+
+    (5, 37),
+    (7, 0)
 ]
 J = [x[0] + round(x[1] / 60, 3) for x in uren_J]
 
@@ -82,43 +122,61 @@ uren_A = [
     (4, 10),
     (0, 0),
     (0, 0),
-    (6, 00)
+
+    (6, 00),
+    (4, 42),
+    (10, 44),
+    (2, 53),
+    (2, 17),
+    (2, 40),
+    (0, 0),
+
+    (5, 9),
+    (4, 54)
 ]
 A = [x[0] + round(x[1] / 60, 3) for x in uren_A]
 
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 
-def time_senne(ax, y, fontsize=8):
-    ax.plot(x, y, color='orangered')
+def time_senne(ax, y, fontsize=12):
+    ax.plot_date(x, y, "o-", color='orangered', label='Senne')
 
     ax.locator_params(nbins=3)
     ax.set_xlabel('datum', fontsize=fontsize)
     ax.set_ylabel('gewerkte uren per dag', fontsize=fontsize)
     ax.set_title('Senne', fontsize=fontsize)
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%Y'))
+    ax.tick_params(rotation=45)
 
-def time_milan(ax, y, fontsize=8):
-    ax.plot(x, y, color='blueviolet')
+def time_milan(ax, y, fontsize=12):
+    ax.plot_date(x, y, "o-", color='blueviolet', label='Milan')
 
     ax.locator_params(nbins=3)
     ax.set_xlabel('datum', fontsize=fontsize)
     ax.set_ylabel('gewerkte uren per dag', fontsize=fontsize)
     ax.set_title('Milan', fontsize=fontsize)
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%Y'))
+    ax.tick_params(rotation=45)
 
-def time_jari(ax, y, fontsize=8):
-    ax.plot(x, y, color='dodgerblue')
+def time_jari(ax, y, fontsize=12):
+    ax.plot_date(x, y, "o-", color='dodgerblue', label='Jari')
 
     ax.locator_params(nbins=3)
     ax.set_xlabel('datum', fontsize=fontsize)
     ax.set_ylabel('gewerkte uren per dag', fontsize=fontsize)
     ax.set_title('Jari', fontsize=fontsize)
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%Y'))
+    ax.tick_params(rotation=45)
 
-def time_alessia(ax, y, fontsize=8):
-    ax.plot(x, y, color='hotpink')
+def time_alessia(ax, y, fontsize=12):
+    ax.plot_date(x, y, "o-", color='hotpink', label='Aléssia')
 
     ax.locator_params(nbins=3)
     ax.set_xlabel('datum', fontsize=fontsize)
     ax.set_ylabel('gewerkte uren per dag', fontsize=fontsize)
     ax.set_title('Aléssia', fontsize=fontsize)
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%Y'))
+    ax.tick_params(rotation=45)
 
 def all_times(ax, S, M, J, A, fontsize=12):
         ax.plot_date(x, S, "o-", color='orangered', label='Senne')
@@ -151,7 +209,7 @@ avgM = sum(M) / len(x)
 avgJ = sum(J) / len(x)
 avgA = sum(A) / len(x)
 print(f"\nGEMIDDELDE UREN:")
-print(f"\tGemiddelde gewerkte uren van Senne: {avgS}\n\tGemiddelde gewerkte uren van Milan: {avgS}\n\tGemiddelde gewerkte uren van Jari: {avgS}\n\tGemiddelde gewerkte uren van Aléssia: {avgS}")
+print(f"\tGemiddelde gewerkte uren van Senne: {avgS}\n\tGemiddelde gewerkte uren van Milan: {avgM}\n\tGemiddelde gewerkte uren van Jari: {avgJ}\n\tGemiddelde gewerkte uren van Aléssia: {avgA}")
 
 ##### totaal per persoon
 totS = sum(S)
@@ -183,6 +241,27 @@ plt.show()
 # gewerkte uren van alle leden te samen in 1 grafiek
 fig, ax = plt.subplots()
 total_time(ax, S, M, J, A)
+plt.tight_layout()
+plt.show()
+
+# chart van elk lid met gewerkte uren over alle weken
+fig, ax = plt.subplots()
+time_senne(ax, S)
+plt.tight_layout()
+plt.show()
+
+fig, ax = plt.subplots()
+time_milan(ax, M)
+plt.tight_layout()
+plt.show()
+
+fig, ax = plt.subplots()
+time_jari(ax, J)
+plt.tight_layout()
+plt.show()
+
+fig, ax = plt.subplots()
+time_alessia(ax, A)
 plt.tight_layout()
 plt.show()
 
